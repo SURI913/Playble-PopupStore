85f8d316ec1e0b69f4e15a6ee68ce207a9d57856
SURI <101981952+SURI913@users.noreply.github.com>
2023-11-22 02:54:14 +0900

    서버 연결 준비

--- Assets/Script/OpenScene.cs
+++ Assets/Script/OpenScene.cs
@@ -2,42 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OpenScene : MonoBehaviour
 {
     [SerializeField] string SceneName;
 
     private bool isOpenServer = false;
+    private Button hostButton;
+    private Button clientButton;
+
 
     private void Awake()
     {
         //서버가 열려있는지 표시하는 UI
+        hostButton = transform.GetChild(0).GetComponent<Button>();
+        hostButton.onClick.AddListener(HostButtonClick);
+        clientButton = transform.GetChild(1).GetComponent<Button>();
+        clientButton.onClick.AddListener(ClientServer);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    //게임 매니저에서 값 받아와야하나
+
+    private void HostButtonClick()
     {
-        if(collision.collider.tag == "Player")
+        if (!isOpenServer)
         {
-            //플레이어일때 실행
-
-            if (isOpenServer)
-            {
-                //서버가열렸다면 
-                //해당 서버로 입장 => 포톤 서버 입장하는 스크립트 추가
-            }
-            else
-            {
-                // 포톤 서버 생성 스크립트 추가
-                OpenServer();
-
-
-            }
+            isOpenServer=true;
+            // 포톤 서버 생성 스크립트 추가 
+            //해당 서버로 입장 => 포톤 서버 입장하는 스크립트 추가
+            ClientServer();
         }
-        //서버 열기 or 서버 들어가기
+        else
+        {
+            Debug.Log("서버가 이미 열린 상태");
+        }
     }
 
-    private  void OpenServer()
+
+    private  void ClientServer()
     {
-        if(SceneName != null)
+        if(SceneName != null && isOpenServer)
         {
             isOpenServer = true;
             SceneManager.LoadScene(SceneName);
