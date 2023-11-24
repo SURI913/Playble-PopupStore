f46b437b19c4461cee52492fecb703764fb7d0e2
JeonJuSeong <84757254+JeonJuSeong@users.noreply.github.com>
2023-11-24 13:33:50 +0900

    update: localhost -> 서버주소, ipfs 해시값 관련

--- Assets/UI/ShopItem.cs
+++ Assets/UI/ShopItem.cs
@@ -58,8 +58,13 @@ public class ShopItem : MonoBehaviour
     {
         //해당 URL은 NFT 발급한 해시값+아이디로 check요청합니다.
         //해당 NFT가 존재할 경우에 "0x0A2..."형식의 주소값이 반환되구요,
-        //없다면 string으로 false라는 문자열이 반환됩니다. 해당 값이 result에 담기게 됩니다.
-        string url = "http://localhost:30001/check?itemname=" + "검사해야할itemname";
+        //없다면 string으로 false라는 문자열이 반환되고, 해당 값이 result에 담기게 됩니다.
+        //이미지는 "https://ipfs.io/ipfs/QmYb6maCE5iSm12WxEYLQAs5wogvGi52Q2w3jXG52qjWFo?filename=Ticket.jpg"
+        //해당 링크에 요청을 쏘면 받을 수 있는데, "?" 뒤의 값은 선택사항이라서,
+        //"http://ipfs.io/ipfs/"링크에 뒤에 해시값을 붙여서 웹으로 쏘면 이미지를 받을 수 있는 구조입니다.
+
+        string url = "http://115.85.181.212:30001/check?itemname=" + "QmYb6maCE5iSm12WxEYLQAs5wogvGi52Q2w3jXG52qjWFo"
+            + "유저ID번호";
 
         UnityWebRequest www = UnityWebRequest.Get(url);
 
@@ -87,13 +92,12 @@ public class ShopItem : MonoBehaviour
     {
         StartCoroutine(SendRequest());
 
-
         //해당 minturl이 mint하는데 필요한 값들을 포함해서 접속하면 되는 URL입니다.
         //NFT아이템 이름은 유저에게 보이는 이름이니까 그냥 XX MUSEUM TICKET 정도여도 될거 같습니다.
         //아이템 해시값은 이미지 하나 찾아서 업로드하고 해시값 보내드리겠습니다.
         //유저ID번호는 낮에 이야기했던 유저 각 고유 ID번호를 붙이면 될 것 같습니다.
-        string minturl = "http://localhost:30001/mint?itemname=" + "NFT아이템이름(유저에게 보임)"
-            + "&itemhash=" + "아이템해시값(Qm..)" + "유저ID번호";
+        string minturl = "http://115.85.181.212:30001/mint?itemname=" + "NFT아이템이름(유저에게 보임)"
+            + "&itemhash=" + "QmYb6maCE5iSm12WxEYLQAs5wogvGi52Q2w3jXG52qjWFo" + "유저ID번호";
 
         //위의 URL사용해서 OpenURL로 웹브라우저 열면 mint하는 페이지로 이동합니다!
         Application.OpenURL(minturl);
