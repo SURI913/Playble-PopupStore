9cac1dde18004d6d64560f1350eb1806cf9a42cc
Ahri <ahri.public@gmail.com>
2023-12-08 02:54:56 +0900

    Photon 멀티 작업 0.1
    로비에서 멀티 연결
    에러 핸들링 필요

--- Assets/Network/NetworkManager.cs
+++ Assets/Network/NetworkManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10dddd6fe96623403bbc373e3986bc639bd0d99f0e7e230deb6af3986aaa483a
-size 5087
+oid sha256:18ae1f43535474480b82901d5704faa3835c12b6523c24036525117aa09d7621
+size 6042
--- Assets/Player/CharacterAnimation/PlayerController.cs
+++ Assets/Player/CharacterAnimation/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76cc4c7ad5d316d20a8ed7c3f10ff2e38356d955552a1e45b4a8ced132644f71
-size 3151
+oid sha256:fc6c34657c050315aae156d4d50b97fcb7812ed5020508e2f29e8f060e37e20b
+size 5234
